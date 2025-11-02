cf9462bb4124f43576364e916364cfa2dc2ebc89
Kurganov1993 <kurganov.4n@yandex.ru>
2025-11-02 17:18:28 +0300

    sda

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    serverComponentsExternalPackages: ['node-fetch', 'https'],
+    serverComponentsExternalPackages: ['https', 'uuid'],
   },
 }
 
