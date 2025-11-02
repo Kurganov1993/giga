05ee57204483a6ccbb434f04b600de214cfa15f7
Kurganov1993 <kurganov.4n@yandex.ru>
2025-11-02 16:52:34 +0300

    деп2

--- next.config.js
+++ next.config.js
@@ -1,29 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    serverComponentsExternalPackages: ['uuid'],
+    serverComponentsExternalPackages: ['uuid', 'node-fetch', 'https-proxy-agent'],
   },
-  serverRuntimeConfig: {
-    apiTimeout: 60000,
-  },
-  api: {
-    bodyParser: {
-      sizeLimit: '4mb',
-    },
-    responseLimit: '4mb',
-  },
-  async headers() {
-    return [
-      {
-        source: '/api/(.*)',
-        headers: [
-          { key: 'Access-Control-Allow-Credentials', value: 'true' },
-          { key: 'Access-Control-Allow-Origin', value: '*' },
-          { key: 'Access-Control-Allow-Methods', value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT' },
-          { key: 'Access-Control-Allow-Headers', value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version' },
-        ]
-      }
-    ]
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals.push('https', 'http');
+    }
+    return config;
   }
 }
 
